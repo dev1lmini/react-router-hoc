49b75fec5240dd1d8b9f47544290c31c3e9398db
Markiian Linynskyi <devilmini.ru@gmail.com>
2021-03-10 10:58:21 +0200

    feat(query): Add initial query params validation class
    * use query params types
    * add types for query params validation

--- src/types/route.d.ts
+++ src/types/route.d.ts
@@ -4,7 +4,7 @@ import type {
   RouteComponentProps,
   match as RouteMatch
 } from "react-router"
-import type { ParamsValidation } from "../validations"
+import type { ParamsValidation, QueryParamsValidation } from "../validations"
 import type * as H from "history"
 import type { SubType, PickType } from "./extracts"
 
@@ -12,8 +12,15 @@ export type ParamsType<T> = {
   [K in keyof T]: T[K] extends ParamsValidation<infer P> ? P : never
 }
 
+export type QueryParamsType<T> = {
+  [K in keyof T]: T[K] extends QueryParamsValidation<infer P> ? P : never
+}
+
 export type Params<T> = SubType<ParamsType<SubType<T, ParamsValidation>>, {}> &
   Partial<PickType<ParamsType<SubType<T, ParamsValidation>>, undefined>>
+
+export type QueryParams<T> = SubType<QueryParamsType<SubType<T, QueryParamsValidation>>, {}> &
+Partial<PickType<QueryParamsType<SubType<T, QueryParamsValidation>>, undefined>>
 
 export type RouteLink<
   Path extends string | undefined,
@@ -28,12 +35,18 @@ type Match<Params = {}> = Omit<RouteMatch<Params>, "params"> & {
   params: Params
 }
 
+type Location<S, Q> =  Omit<H.Location<S>, 'search'> & {
+  search: keyof Q extends never ? H.Search : Q
+};
+
 export type RouteHOCProps<
   Path extends string | undefined = undefined,
   Params = {},
+  QueryParams = {},
   C extends StaticContext = StaticContext,
   S = H.LocationState
-> = Omit<RouteComponentProps<Params, C, S>, "match"> & {
+> = Omit<RouteComponentProps<Params, C, S>, "match" | "location"> & {
   match: Match<Params>
   link: RouteLink<Path, Params>
+  location: Location<S, QueryParams>
 }
