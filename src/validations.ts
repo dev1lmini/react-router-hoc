8e1da5be7dd3a8c77c844864746a95d9d567bd7b
Markiian Linynskyi <devilmini.ru@gmail.com>
2021-03-11 11:13:34 +0200

    feat(query): add query params validation
    Add validation class for query params. Perform validation for queries based on validation schema.
    If a value match validation schema it's available in the match query props. If it's not valid the value is undefined.
    If the validation schema requires some value, it will be populated if it's not matched the schema or it's not exists.

--- src/validations.ts
+++ src/validations.ts
@@ -1,13 +1,12 @@
-import type { ExtractArray } from "./types";
+import type { ExtractArray } from "./types"
 
 enum RulesEnum {
   STRING = "string",
   NUMBER = "number",
   ENUM = "enum",
   REGEX = "regex",
-  OPTIONAL = "optional",
+  OPTIONAL = "optional"
 }
-
 
 enum QueryParamsType {
   STRING = "string",
@@ -23,22 +22,22 @@ export class ParamsValidation<T = never> {
     [P in RulesEnum]: (
       rule: string | Array<number | string>,
       path?: string
-    ) => string;
+    ) => string
   } & {
-    init: (rule: string) => string;
+    init: (rule: string) => string
   } = {
-    init: (rule) => `:${rule}`,
-    string: (rule) => rule.toString(),
-    number: (rule) => `${rule}(\\d+)`,
+    init: rule => `:${rule}`,
+    string: rule => rule.toString(),
+    number: rule => `${rule}(\\d+)`,
     enum: (rule, path) =>
       `${Array.isArray(rule) ? `:${path}(${rule.join("|")})` : rule}`,
     regex: (rule, path) => `:${path}(${rule.toString().slice(1, -1)})`,
-    optional: (rule) => `${rule}?`,
-  };
+    optional: rule => `${rule}?`
+  }
 
   constructor(
     private _rules?: {
-      [P in RulesEnum]: boolean | Array<number | string> | RegExp;
+      [P in RulesEnum]: T | boolean | Array<number | string> | RegExp
     }
   ) {
     this._rules = this._rules ?? {
@@ -46,8 +45,8 @@ export class ParamsValidation<T = never> {
       number: false,
       enum: [],
       regex: false,
-      optional: false,
-    };
+      optional: false
+    }
   }
 
   generateRules = (paramKey: string) => {
@@ -56,103 +55,252 @@ export class ParamsValidation<T = never> {
       Object.entries(this._rules).reduce((acc: string, [key, item]) => {
         switch (true) {
           case !!(Array.isArray(item) && item.length):
-            return this.regexes[key as RulesEnum](item as any, paramKey);
+            return this.regexes[key as RulesEnum](item as any, paramKey)
           case item instanceof RegExp:
-            return this.regexes[key as RulesEnum](item as any, paramKey);
+            return this.regexes[key as RulesEnum](item as any, paramKey)
           case item:
-            return this.regexes[key as RulesEnum](acc);
+            return this.regexes[key as RulesEnum](acc)
           default:
-            return acc;
+            return acc
         }
       }, this.regexes.init(paramKey))
-    );
-  };
+    )
+  }
 
   enum<Keys extends Array<number | string>>(...keys: Keys) {
     if (!this._rules) {
-      throw Error(`Validation hasn't been initialized`);
+      throw Error(`Validation hasn't been initialized`)
     }
     return new ParamsValidation<T | ExtractArray<Keys>>({
       ...this._rules,
-      enum: keys,
-    });
+      enum: keys
+    })
   }
 
   get string() {
     if (!this._rules) {
-      throw Error(`Validation hasn't been initialized`);
+      throw Error(`Validation hasn't been initialized`)
     }
-    return new ParamsValidation<T | string>({ ...this._rules, string: true });
+    return new ParamsValidation<T | string>({ ...this._rules, string: true })
   }
 
   regex(reg: RegExp) {
     if (!this._rules) {
-      throw Error(`Validation hasn't been initialized`);
+      throw Error(`Validation hasn't been initialized`)
     }
-    return new ParamsValidation<T | string>({ ...this._rules, regex: reg });
+    return new ParamsValidation<T | string>({ ...this._rules, regex: reg })
   }
 
   get number() {
     if (!this._rules) {
-      throw Error(`Validation hasn't been initialized`);
+      throw Error(`Validation hasn't been initialized`)
     }
-    return new ParamsValidation<T | number>({ ...this._rules, number: true });
+    return new ParamsValidation<T | number>({ ...this._rules, number: true })
   }
 
   get optional() {
     if (!this._rules) {
-      throw Error(`Validation hasn't been initialized`);
+      throw Error(`Validation hasn't been initialized`)
     }
     return new ParamsValidation<T | undefined>({
       ...this._rules,
-      optional: true,
-    });
+      optional: true
+    })
+  }
+}
+
+export class QueryParam<T> {
+  constructor(private value: T, private required: boolean = false) {}
+
+  get isRequired() {
+    return this.required
+  }
+
+  valueOf(deep = false) {
+    const value = (this.value as any)?.valueOf()
+    if (deep && Array.isArray(value)) {
+      return value.map(item => item?.valueOf())
+    }
+    return value
   }
 }
 
 export class QueryParamsValidation<T = never> {
+  private initialRules = {
+    string: false,
+    number: false,
+    boolean: false,
+    enum: [],
+    array: false,
+    regex: false,
+    required: false
+  }
+  private validators = {
+    string: (value: any) => {
+      if (typeof value === "string") {
+        return new QueryParam(value)
+      }
+    },
+    number: (value: any) => {
+      const number = Number(value)
+      if (!Number.isNaN(number) && value !== null) {
+        return new QueryParam(number)
+      }
+    },
+    boolean: (value: any) => {
+      if (value) {
+        try {
+          const bool = JSON.parse(value)
+          if (typeof bool === "boolean") return new QueryParam(bool)
+        } catch {
+          return
+        }
+      }
+    },
+    enum: (value: any, values: any) => {
+      if (Array.isArray(values)) {
+        return new QueryParam(values.find(item => item == value))
+      }
+    },
+    array: (value: any, type: any) => {
+      if (
+        Array.isArray(value) &&
+        value.length &&
+        type instanceof QueryParamsValidation
+      ) {
+        return new QueryParam(value.map(item => type.validate([item])))
+      }
+    },
+    regex: (value: any, rule: any) => {
+      if (rule instanceof RegExp && rule.test(value)) {
+        return new QueryParam(value)
+      }
+    },
+    required: (value: any) => {
+      if (value === undefined || value === null) {
+        return new QueryParam(this._rules?.required, true)
+      }
+      return new QueryParam(value)
+    }
+  }
   constructor(
     private _rules?: {
-      [P in QueryParamsType]: boolean | Array<number | string> | RegExp;
+      [P in QueryParamsType]:
+        | boolean
+        | Array<number | string>
+        | RegExp
+        | QueryParamsValidation<any>
     }
   ) {
-    this._rules = this._rules ?? {
-      string: false,
-      array: false,
-      number: false,
-      boolean: false,
-      enum: [],
-      regex: false,
-      required: false,
-    };
+    this._rules = this._rules ?? this.initialRules
   }
 
-  required<K extends T>(_defaultParam: K): QueryParamsValidation<NonNullable<T>> {
-    return this
+  validate(value: any[]): QueryParam<T> {
+    const rules =
+      this._rules &&
+      ((Object.entries(this._rules).filter(([_key, item]) =>
+        Array.isArray(item) ? item.length : item
+      ) as unknown) as
+        | [QueryParamsType, boolean | Array<number | string> | RegExp][]
+        | undefined)
+
+    return rules?.reduce<QueryParam<any> | undefined>(
+      (acc, [key, item], index, array) => {
+        const validator = this.validators[key]
+
+        const value =
+          key !== QueryParamsType.ARRAY &&
+          array[index - 1]?.[0] !== QueryParamsType.ARRAY &&
+          Array.isArray(acc)
+            ? acc[acc.length - 1]
+            : acc
+
+        const queryParam = validator(value, item)
+
+        if (index === array.length - 1) {
+          return queryParam
+        }
+        return queryParam?.valueOf()
+      },
+      (value as unknown) as QueryParam<any>
+    ) as QueryParam<T>
   }
 
-  array<K extends QueryParamsValidation>(type: K): QueryParamsValidation<T | Array<K extends QueryParamsValidation<infer V> ? V : any> | undefined> {
-    return this
+  required<K extends T>(defaultParam: NonNullable<K>) {
+    if (!this._rules) {
+      throw Error(`Validation hasn't been initialized`)
+    }
+    return new QueryParamsValidation<NonNullable<T>>({
+      ...this._rules,
+      required: defaultParam as any
+    })
   }
 
-  get number(): QueryParamsValidation<T | number | undefined> {
-    return this
+  array<K extends QueryParamsValidation<any>>(type: K) {
+    if (!this._rules) {
+      throw Error(`Validation hasn't been initialized`)
+    }
+    return new QueryParamsValidation<
+      T | Array<K extends QueryParamsValidation<infer V> ? V : any> | undefined
+    >({
+      ...this.initialRules,
+      array: type,
+      required: this._rules.required
+    })
   }
 
-  get string(): QueryParamsValidation<T | string | undefined> {
-    return this
+  get number() {
+    if (!this._rules) {
+      throw Error(`Validation hasn't been initialized`)
+    }
+    return new QueryParamsValidation<T | number | undefined>({
+      ...this.initialRules,
+      number: true,
+      required: this._rules.required
+    })
   }
 
-  get boolean(): QueryParamsValidation<T | boolean | undefined> {
-    return this
+  get string() {
+    if (!this._rules) {
+      throw Error(`Validation hasn't been initialized`)
+    }
+    return new QueryParamsValidation<T | string | undefined>({
+      ...this.initialRules,
+      string: true,
+      required: this._rules.required
+    })
   }
 
-  get regex(): QueryParamsValidation<T | string | undefined> {
-    return this
+  get boolean() {
+    if (!this._rules) {
+      throw Error(`Validation hasn't been initialized`)
+    }
+    return new QueryParamsValidation<T | boolean | undefined>({
+      ...this.initialRules,
+      boolean: true,
+      required: this._rules.required
+    })
   }
 
-  enum<Keys extends Array<number | string>>(...keys: Keys): QueryParamsValidation<T | ExtractArray<Keys> | undefined> {
-    return this
+  regex(rule: RegExp) {
+    if (!this._rules) {
+      throw Error(`Validation hasn't been initialized`)
+    }
+    return new QueryParamsValidation<T | string | undefined>({
+      ...this.initialRules,
+      regex: rule,
+      required: this._rules.required
+    })
+  }
+
+  enum<Keys extends Array<number | string>>(...keys: Keys) {
+    if (!this._rules) {
+      throw Error(`Validation hasn't been initialized`)
+    }
+    return new QueryParamsValidation<T | ExtractArray<Keys> | undefined>({
+      ...this.initialRules,
+      enum: keys,
+      required: this._rules.required
+    })
   }
 }
-
