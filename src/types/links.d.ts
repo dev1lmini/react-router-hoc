75af1a5ca754ddee3565c2e698e5770d38c9fafa
Markiian Linynskyi <devilmini.ru@gmail.com>
2021-03-12 17:46:41 +0200

    feat(links): add query params to the link
    Add possibility to provide query params for the exact route using link function

--- src/types/links.d.ts
+++ src/types/links.d.ts
@@ -1,7 +1,7 @@
 
 export type RouteWithLink = {
   [x: string]: any
-  link: ((params: any) => string) | string;
+  link: ((params: any) => string) | string | undefined;
 }
 
-export type GetLink<T> = T extends RouteWithLink ? T['link'] : string
+export type GetLink<T> = T extends RouteWithLink ? NonNullable<T['link']> : string
