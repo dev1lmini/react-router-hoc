3ad359fb61fd3e22c3edb155d64dc15c434210ce
Markiian Linynskyi <devilmini.ru@gmail.com>
2021-04-09 16:38:20 +0300

    parent 41a05a2fc4d13247d52d773a0368835f8db26c5d author Markiian Linynskyi <devilmini.ru@gmail.com> 1617975500 +0300 committer Markiian Linynskyi <devilmini.ru@gmail.com> 1617998472 +0300
    feat(documentation): add new documentation regarding new query params
    
    * add documentation regarding query params
    * add examples for query and path params
    * add example for using link internally

--- examples/src/App.tsx
+++ examples/src/App.tsx
@@ -1,23 +1,25 @@
-import React from "react";
-import { Router, Switch } from "react-router";
-import "./App.css";
-import { createBrowserHistory } from "history";
-import { Link } from "react-router-dom";
-import Search from "./routes/Search";
-import Dashboard from "./routes/Dashboard";
-import Home from "./routes/Home";
-import NotFound from "./routes/NotFound";
-import Article from "./routes/Article";
-import { getLinks } from "react-router-hoc";
+import React from "react"
+import { Router, Switch } from "react-router"
+import "./App.css"
+import { createBrowserHistory } from "history"
+import { Link } from "react-router-dom"
+import Search from "./routes/Search"
+import Dashboard from "./routes/Dashboard"
+import Home from "./routes/Home"
+import NotFound from "./routes/NotFound"
+import Article from "./routes/Article"
+import { getLinks } from "react-router-hoc"
+import Overview from "./routes/Overview"
 
-const history = createBrowserHistory();
+const history = createBrowserHistory()
 
 export const links = getLinks({
   Dashboard,
   Home,
   Search,
   Article,
-});
+  Overview
+})
 
 function App() {
   return (
@@ -27,18 +29,28 @@ function App() {
           <Switch>
             <Home exact />
             <Search />
-            <Dashboard access={false} />
+            <Dashboard access={true} />
             <Article />
+            <Overview />
             <NotFound />
           </Switch>
           <Link to={links.Home()}>Home</Link>
-          <Link to={links.Dashboard({ role: "customer" })}>Dashboard</Link>
+          <Link
+            to={links.Dashboard({
+              role: "customer",
+              region: "Staryy Sambir",
+              gangs: ["grove_street"]
+            })}
+          >
+            Dashboard
+          </Link>
           <Link to={links.Search({ city: "Lviv" })}>Search</Link>
+          <Link to={links.Overview({ exapnded: true})}>Overview</Link>
           <Link to={links.Article({ id: 10 })}>Article #10</Link>
         </Router>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- examples/src/routes/Dashboard.tsx
+++ examples/src/routes/Dashboard.tsx
@@ -1,16 +1,40 @@
-import React from "react";
-import { Route, compose } from "react-router-hoc";
-import { ProtectedRoute } from "../utils/hocs";
+import React from "react"
+import { Route, compose } from "react-router-hoc"
+import { ProtectedRoute } from "../utils/hocs"
 
 const DashboardRoute = compose(
   ProtectedRoute,
   Route(
     {
       role: Route.params.oneOf("customer", "vendor"),
+      region: Route.params.string,
+      storage: Route.params.number.optional,
+      page: Route.query.number,
+      range: Route.query.oneOf("day", "week"),
+      gangs: Route.query.array(
+        Route.query.oneOf("ballas", "grove_street", "lost_santos")
+      )
     },
-    ({ role }) => `/dashboard/${role}`
+    ({ role, region, storage }) => `/dashboard/${role}/${region}/${storage}`
   )
-);
-export default DashboardRoute(({ match: { params: { role } } }) => (
-  <div>Dashboard for {role}</div>
-));
+)
+export default DashboardRoute(
+  ({
+    match: {
+      params: { role, region, storage },
+      query: { gangs, range = "day", page = 1 }
+    }
+  }) => (
+    <div>
+      Dashboard for
+      <div>
+        {role} | {region} | {storage}
+      </div>
+      <div>
+        {gangs?.join("|")}<br/>
+        Range: {range}<br/>
+        Page: {page}<br/>
+      </div>
+    </div>
+  )
+)
